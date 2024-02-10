6145d74c630c348f2380f86ef67d34472af8c7d9
AndresOnate <andrescamiloquimbayo@gmail.com>
2024-02-10 08:54:36 -0500

    Postgres Conexion

---
+++ src/main/java/com/edu/eci/ieti/PostgresSQL/PostgresSqlApplication.java
@@ -0,0 +1,13 @@
+package com.edu.eci.ieti.PostgresSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/eci/ieti/PostgresSQL/PostgresSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.eci.ieti.PostgresSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
